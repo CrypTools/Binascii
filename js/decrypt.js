77a46f2b2a0404b1593a2890dbd3414d50323195
Lucas Gruwez <gruwez.lucas@gmail.com>
2020-10-11 16:04:08 +1100

    Update js to throw error when non-ascii character is passed.

--- js/decrypt.js
+++ js/decrypt.js
@@ -1,13 +1,21 @@
-/******************************
+/* ==========================================================================
+ *
+ *    Use:
+ *    "01001000 01100101 01101100 01101100 01101111 00100000 01010111 01101111 01110010 01101100 01100100 00100001".decrypt()
+ *    => "Hello World!"
+ *
+ * ========================================================================== */
 
-Use : "message".encrypt().decrypt()
-
-******************************/
 
 String.prototype.decrypt = function() {
-	return this.replace(/\s*[01]{8}\s*/g, function(bin) {
-        return String.fromCharCode(parseInt(bin, 2))
-    })
+
+	return this.replace(/\s*[01]{8}\s*/g, bin => {
+
+        let charCode = parseInt(bin, 2);
+        return String.fromCharCode(charCode);
+
+    });
+
 }
 
-module.exports = text => text.decrypt()
+module.exports = text => text.decrypt();
